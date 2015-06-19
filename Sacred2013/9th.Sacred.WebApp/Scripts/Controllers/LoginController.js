ad8b9ab38f9e156e9b6b2b450b66d0bba34d8f01
Shadess <tylermboettcher@gmail.com>
2015-06-18 17:19:21 -0700

    Adding AngularJS

---
+++ Sacred2013/9th.Sacred.WebApp/Scripts/Controllers/LoginController.js
@@ -0,0 +1,15 @@
+﻿var LoginController = function ($scope, $http, $window) {
+    $scope.LoginErrorMessage = '';
+
+    $scope.login = function (user) {
+        var validateUrl = $window.API_URL + "User/ValidateUser?email=" + user.email + "&password=" + user.password;
+        
+        $http.get(validateUrl).success(function (data) {
+            console.log(data);
+            if (!data.Success) {
+                // Bad login
+                $scope.LoginErrorMessage = data.Message;
+            }
+        });
+    };
+}
---
+++ Sacred2013/9th.Sacred.WebApp/Scripts/Sacred/SacredApp.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    var app = angular.module('SacredApp', ['SacredHomeApp']);
+
+    app.controller('LoginController', ['$scope', '$http', '$window', LoginController]);
+})();
